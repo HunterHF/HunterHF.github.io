fc71470a10c779650993ef146d0f8e2cd9fa6055
HunterHF <53719535+HunterHF@users.noreply.github.com>
2019-08-05 09:15:37 +0300

    Update contactform.js

--- contactform.js
+++ contactform.js
@@ -5,7 +5,7 @@ var str = $(this).serialize();
 
 $.ajax({
 type: "POST",
-url: "contact.php",
+url: "https://hunterhf.github.io/contact.php",
 data: str,
 success: function(msg) {
 if(msg == 'OK') {
